b3f5cf8b77fe33208c1e4f8ad159df906e2fc8d7
Maria Clara <maria.clara@wisereducacao.com>
2023-03-27 15:03:29 -0300

    iniciando novo projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{ 
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        React Native
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Segunda, 27 de março de 2023
+      </Text>
     </View>
-  );
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
