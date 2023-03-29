ceabca708a0b519e016ce292cb6e271157df9ee2
Maria Clara <maria.clara@wisereducacao.com>
2023-03-28 22:43:18 -0300

    refatorando e usando stylesheet

--- App.tsx
+++ App.tsx
@@ -1,27 +1,28 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App(){
   return(
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{ 
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
-        React Native
-      </Text>
-
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16
-      }}>
-        Segunda, 27 de março de 2023
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}> React Native </Text>
+      <Text style={styles.eventDate}> Segunda, 27 de março de 2023 </Text>
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+});
