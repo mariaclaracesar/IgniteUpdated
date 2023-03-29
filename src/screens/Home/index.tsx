6f79fb4ae218ebb1ebfd0dec294d8886be1d04ea
Maria Clara <maria.clara@wisereducacao.com>
2023-03-28 23:09:44 -0300

    utilizando textInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from 'react-native'
+import { Text, View, TextInput } from 'react-native'
 
 import { styles } from './styles'
 
-export default function Home(){
+export function Home(){
   return(
     <View style={styles.container}>
       <Text style={styles.eventName}> 
@@ -12,6 +12,12 @@ export default function Home(){
       <Text style={styles.eventDate}>
          Segunda, 27 de março de 2023
       </Text>
+
+      <TextInput 
+        style={styles.input} 
+        placeholder="Nome do participante"
+        placeholderTextColor="#CBCBCB"
+      />
     </View>
     )
 }
