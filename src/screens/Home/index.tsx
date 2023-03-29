7c1df42b016b74a27241f24606c64f8393ee502c
Maria Clara <maria.clara@wisereducacao.com>
2023-03-28 23:28:25 -0300

    adicionando button de adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity} from 'react-native'
 
 import { styles } from './styles'
 
 export function Home(){
+
+  function handleParticipantAdd(){
+    console.log('VOCE TA CLICANDO AQUIIIIII')
+  }
+
   return(
     <View style={styles.container}>
       <Text style={styles.eventName}> 
@@ -13,11 +18,19 @@ export function Home(){
          Segunda, 27 de março de 2023
       </Text>
 
+    <View style={styles.form}>
       <TextInput 
-        style={styles.input} 
-        placeholder="Nome do participante"
-        placeholderTextColor="#CBCBCB"
+          style={styles.input} 
+          placeholder="Nome do participante"
+          placeholderTextColor="#CBCBCB"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+            +
+        </Text>
+      </TouchableOpacity>
     </View>
-    )
+  </View>
+  )
 }
