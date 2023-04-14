9d63c7ee1ac0052d4088b0bd9d355aeabb712a70
Maria Clara <mariaclaracesar1_5@hotmail.com>
2023-04-14 18:42:09 -0300

    finalizando o app

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,36 @@
-import { Alert } from 'react-native'
-import { Text, View, TextInput, TouchableOpacity, FlatList} from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert} from 'react-native'
 import { Participant } from '../../components/Participant'
 
 import { styles } from './styles'
 
 export function Home(){
-  const participants = ['Rodrigo', 'Vini', 'Diego', 'Maria', 'Marcus', 'Hud', 'Lucas', 'Mike', 'Monica', 'Pheobe']
-
+  // lista de participantes
+ const [participants, setParticipants] = useState<string[]>([]);
+ const [participantName, setParticipantName] = useState('');
+ 
   function handleParticipantAdd(){
-    if(participants.includes("Rodrigo")){
+    //O método includes() determina se um conjunto de caracteres pode ser encontrado dentro de outra string, retornando true ou false .
+    if(participants.includes(participantName)){
       return Alert.alert("Participante existe", 'Já existe um participante na lista com esse nome')
     }
+
+    // usando o estado para adicionar na lista
+		// recupera o estado anterior e junta em um nome array com o novo participante
+    setParticipants(prevState => [...prevState, participantName]);
+    //limpando o estado voltando para um conteudo vazio
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string){
+    //o filter no estado de participantes(que é um array) tem uma condição de que ele retorne todos o participantes que 
+    //o nome desse particpante(que ta dentro do array) é diferente desse nome que eu to querendo deletar 
+
+    // mensagem de confirmação
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
@@ -29,11 +42,11 @@ export function Home(){
   return(
     <View style={styles.container}>
       <Text style={styles.eventName}> 
-        React Native
+        Adicionar Participantes
       </Text>
 
       <Text style={styles.eventDate}>
-         Segunda, 27 de março de 2023
+         Segunda, 17 de abil de 2023
       </Text>
 
     <View style={styles.form}>
@@ -41,6 +54,10 @@ export function Home(){
           style={styles.input} 
           placeholder="Nome do participante"
           placeholderTextColor="#CBCBCB"
+          // acessa o conteudo atual da caixa de texto toda vez q ele muda
+          onChangeText={setParticipantName}
+          // limpando o valor do imput
+          value={participantName}
       />
 
       <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
@@ -61,12 +78,13 @@ export function Home(){
           />
       )}
       showsVerticalScrollIndicator={false}
+			// quando a lista estiver vazia vai ser renderizado esse texto
       ListEmptyComponent={() => (
         <Text style={styles.listEmptyText}>
-          Nimguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
         </Text>
       )}
     />
   </View>
-  )
+  );
 }
